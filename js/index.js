e06df18399b03d525337d1a67bafc9b1e2e0cdf0
elmaykhel <73468071+elmaykhel@users.noreply.github.com>
2021-05-06 09:50:57 +0200

    primeros pasos

---
+++ js/index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+
+  var modal = document.getElementById("myModal");
+    console.log("hola");
+    document.getElementById("login").addEventListener("click", function(){
+      console.log("adeu"); 
+      modal.style.display = "block";
+    })
+  };
