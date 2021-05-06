2947a5f7a04e790b83d86ee7e417fb74f8fbfbce
elmaykhel <73468071+elmaykhel@users.noreply.github.com>
2021-05-06 10:34:29 +0200

    cambios minimos

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 window.onload = function() {
 
-  var modal = document.getElementById("myModal");
-    console.log("hola");
+    var modal = document.getElementById("modalLogin");
     document.getElementById("login").addEventListener("click", function(){
-      console.log("adeu"); 
       modal.style.display = "block";
     })
   };
