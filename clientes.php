f883d0b2f9432a7c0e637786ae9f67eb80a63568
elmaykhel <73468071+elmaykhel@users.noreply.github.com>
2021-05-12 12:27:12 +0200

    form funcionando y pagina principal

---
+++ clientes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficha Clientes</title>
+</head>
+<body>
+    <label>Nombre: </label>
+    <input type="text" name="nombre">
+    <br>
+    <label>Apellidos: </label>
+    <input type="text" name="apellidos">
+    <input type="submit" name="searchbtn">
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>L'Essència</title>
+</head>
+<body>
+    <div id="header"></div>
+
+    <div id="body">
+        <div id="lineaCentral">
+            <input type="image" id="img1" src="./img/cliente.png" onclick="location.href='clientes.php'"></input>
+            <button id="cliente"></button>
+            <button id="cliente"></button>
+        </div>
+    </div>
+
+    <div id="footer"></div>
+</body>
+</html>
